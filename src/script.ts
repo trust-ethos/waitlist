b82b79e12f5b969ec41edcf533cc5692a68d023f
Vitalii Saienko <modestfake@gmail.com>
2024-02-04 20:46:54 +0100

    Add analytics

---
+++ src/script.ts
@@ -0,0 +1,11 @@
+import * as amplitude from '@amplitude/analytics-browser';
+
+const apiKey = process.env.AMPLITUDE_API_KEY ?? 'd8e9e6064066bfd2405cfce189c578c6';
+
+amplitude.init(apiKey, {
+  defaultTracking: true,
+});
+
+document.getElementById('open-waitlist')?.addEventListener('click', () => {
+  amplitude.track('click-open-waitlist');
+})
